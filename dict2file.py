59f2eda0425b80adde89d238efc326517517fc15
Thien Ngan Doan <ngandn18@gmail.com>
2022-02-01 10:54:33 -0600

    Update dict2file.py
    Modify function dict_fr_file using json

--- dict2file.py
+++ dict2file.py
@@ -1,17 +1,9 @@
 import ast
-# with open('logs.txt', 'r') as f:
-# #     line = f.readline()
-#     x = ast.literal_eval(f.readline())
-#     print(type(x),'\n', x.keys())
-
-# filename = 'a.txt'
-# my_dict = x
-# with open(filename, 'w') as f:
-#     f.write(str(my_dict))
+import json
 
 def dict_2_file(dic, filename):
     '''
-    dic: dictionary saved to filename
+    dic: dictionary saved to filename by "str(dictionary)"
     '''
     with open(filename, 'w') as f:
         f.write(str(dic))
@@ -22,11 +14,20 @@ def dict_fr_file(filename):
     '''
     filename: file loaded to dic
     '''
-    # import ast
     with open(filename, 'r') as f:
-        return ast.literal_eval(f.readline())
+        return json.loads(f.read().replace("'",'"'))
+    # import ast
+    # with open(filename, 'r') as f:
+    #     return ast.literal_eval(f.readline())
 
 # Testing
 # mydict = dict_fr_file('logs.txt')
-# dict_2_file(mydict, 'a.txt')
-# print(mydict)
+# dict_2_file(mydict, 'b.txt')
+# print(mydict.keys())
+
+# with open('a.txt', 'r') as f:
+#     line = f.read()
+# # line = line.replace("'", '"')
+# dict=json.loads(line.replace("'", '"'))
+# print(type(line),'\n', line)
+# dict.keys()
